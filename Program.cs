cc0e136721d7a8087cb7df083faa509fabf55609
Yaroslaw-aw <manumission@mail.ru>
2023-03-08 17:49:09 +0300

    Task 57 complete + matrix_sort ))

--- Program.cs
+++ Program.cs
@@ -3,14 +3,27 @@
 // встречается элемент входных данных.
 
 
-int lines = new Random().Next(3, 5);
-int columns = new Random().Next(3, 5);
+Console.WriteLine("Введите число строк двумерного массива");
+int lines = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов двумерного массива");
+int columns = int.Parse(Console.ReadLine());
 
 int[,] array_57 = Fill_Array(lines, columns);
 
+PrintArray(array_57);
+
+//Matrix_sort(array_57);
+
+//PrintArray(array_57);
+
 int[] one_d_array = Two_dimensionnal_array_to_one_dementional(array_57);
 
+PrintArray(one_d_array);
+
 Array.Sort(one_d_array);
+
+PrintArray(one_d_array);
 
 List_of_numbers(one_d_array);  // решение задачи
 
@@ -66,29 +79,78 @@ int[,] Fill_Array(int m, int n)
     {
         for (int j = 0; j < _array.GetLength(1); j++)
         {
-            _array[i, j] = new Random().Next(20);            
+            _array[i, j] = new Random().Next(10);            
         }        
     }
     return _array;
 }
 
-void Print_2d_array(int[,] _array)
+
+
+
+//Вывести массив любого типа на консоль
+void PrintArray(Array matrix)
 {
-    for (int i = 0; i < _array.GetLength(0); i++)
+    switch (matrix.Rank)
     {
-        for (int j = 0; j < _array.GetLength(1); j++)
-        {
-            Console.Write(_array[i, j] + " ");
-        }
-        Console.WriteLine();
+        case 1:
+            Console.WriteLine("Ваш массив: ");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                Console.Write($"{matrix.GetValue(i)} ");
+            Console.WriteLine();
+            break;
+        case 2:
+            Console.WriteLine("Ваш массив: ");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix.GetValue(i, j)} ");
+                }
+                Console.WriteLine();               
+            }
+            Console.WriteLine();
+            break;
     }
 }
 
-void Print_1d_array(int[] _array)
-{
-    for (int i = 0; i < _array.Length; i++)
+
+
+void Matrix_sort(int[,] matrix)
+{  
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write(_array[i] + " ");
+        for (int j = 0;j < matrix.GetLength(1); j++)
+        {
+            int last_i = matrix.GetLength(0) - 1 - i;
+            int last_j = matrix.GetLength(1) - 1 - j;
+
+            int max = matrix[last_i, last_j];
+            int max_i = last_i;
+            int max_j = last_j;
+
+            for (int k = 0; k < matrix.GetLength(0) - i; k++)
+            {
+                for (int l = 0; l < matrix.GetLength(1); l++)
+                {                    
+                    if (k == last_i &&
+                        l == matrix.GetLength(1) - j)
+                    {
+                        goto Label;
+                    }
+
+                    if (matrix[k, l] > max)
+                    {
+                        max = matrix[k, l];
+                        max_i = k;
+                        max_j = l;                        
+                    }                    
+                }
+            }
+        Label:
+            int temp = matrix[last_i, last_j];
+            matrix[last_i, last_j] = max;
+            matrix[max_i, max_j] = temp;            
+        }
     }
-    Console.WriteLine();
 }
